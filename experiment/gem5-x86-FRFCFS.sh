8870ca933bb961effeabf7aa69ffed8faf434f72
abj456 <tommy51315@gmail.com>
2025-06-16 22:32:35 +0800

    ext: Store changes in experiment/ directory

--- experiment/gem5-x86-FRFCFS.sh
+++ experiment/gem5-x86-FRFCFS.sh
@@ -5,30 +5,44 @@ if [ -z "$BENCHMARK" ]; then
 	exit 1
 fi
 
-STATS_DIR=~/full_system_images/disks/${BENCHMARK}
+CONFIG_SCRIPT_DIR=./gem5-config-scripts
 
 if [ "$BENCHMARK" == "sysbench" ] ; then
 	SIZE=$2
 	THREADS=$3
-	if [ -z "$SIZE" ] || [ -z "$THREADS" ] ; then
-		echo "Usage: $0 <benchmark> <size> <thread>"
+	SYS_CMD=$4
+	STATS_DIR=$5
+	if [ -z "$SIZE" ] || [ -z "$THREADS" ] || [ -z "$SYS_CMD" ] || [ -z "$STATS_DIR" ]; then
+		echo "Usage: $0 <benchmark> <size> <thread> <sysbench command> <stats dir>"
 		exit 1
 	fi
-	/home/abj456/gem5/build/X86-FRFCFS/gem5.opt \
-		-d ${STATS_DIR}/m5out-FRFCFS-${THREADS}-${SIZE}/ \
-		~/full_system_images/disks/${BENCHMARK}-x86-fs.py \
-		--size=$SIZE --threads=$THREADS
+
+	/home/abj456/gem5/build/X86-FRFCFS/gem5.fast \
+		-d ${STATS_DIR}/ \
+		./${BENCHMARK}-x86-fs.py \
+		--size=$SIZE --threads=$THREADS --sys_cmd="${SYS_CMD}"
 elif [ "$BENCHMARK" == "parsec" ] ; then
-	CHOOSE=$2
-	SIZE=$3
-	if [ -z "$CHOOSE" ] || [ -z "$SIZE" ] ; then
-		echo "Usage: $0 <benchmark> <choose> <size>"
+	SIZE=$2
+	HEAVY_TAIL=$3
+	LIGHT_TAIL=$4
+	STATS_DIR=$5
+	KVM_TICKS=$6
+	TIMING_TICKS=$7
+	if [ -z "$KVM_TICKS" ] || [ -z "$TIMING_TICKS" ]; then
+		KVM_TICKS="0.5e12"
+		TIMING_TICKS="0.5e12"
+	fi
+	if [ -z "$SIZE" ] || [ -z "$HEAVY_TAIL" ] || [ -z "$LIGHT_TAIL" ] || [ -z "$STATS_DIR" ]; then
+		echo "Usage: $0 <benchmark> <size> <heavy> <light> <stats dir> [kvm ticks] [timing ticks]"
 		exit 1
 	fi
-	/home/abj456/gem5/build/X86-FRFCFS/gem5.opt \
-		-d ${STATS_DIR}/m5out-FRFCFS-${CHOOSE}-${SIZE}/ \
-		~/full_system_images/disks/${BENCHMARK}-x86-fs.py \
-		--benchmark=$CHOOSE --size=$SIZE
+
+	/home/abj456/gem5/build/X86-FRFCFS/gem5.fast \
+		-d ./parsec-gem5/m5out-${SIZE}-h${HEAVY_TAIL}-l${LIGHT_TAIL}/frfcfs-${STATS_DIR}/ \
+		${CONFIG_SCRIPT_DIR}/${BENCHMARK}-x86-fs.py \
+		--size=$SIZE --heavy_threads=$HEAVY_TAIL --light_threads=$LIGHT_TAIL \
+		--kvm_ticks=$KVM_TICKS --timing_ticks=$TIMING_TICKS
+
 else
 	echo "Invalid benchmark. Use 'sysbench' or 'parsec'."
 	exit 1
--- experiment/qemu_create_img.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-DISK_FILE=$1
-SIZE=$2
-qemu-img create -f raw $DISK_FILE $SIZE
